204fce061e17559bdaa36f13f5bf0108eb2f42af
vvk130 <vanessa.kasula@outlook.com>
2024-05-22 20:46:07 +0300

    first commit

---
+++ exercises.svelte
@@ -0,0 +1,33 @@
+Statements
+
+<script>
+	let count = 0;
+	$: alert(`the count is ${count}`)
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button on:click={handleClick}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+<p></p>
+
+Updating arrays and objects 
+
+<script>
+	let numbers = [1, 2, 3, 4];
+
+	function addNumber() {
+		numbers = [...numbers, numbers.length +1];
+	}
+
+	$: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0);
+</script>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={addNumber}>
+	Add a number
+</button>
