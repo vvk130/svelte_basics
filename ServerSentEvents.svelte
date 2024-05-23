c4589455b73995af93f569e4bc227480d8cb09e9
vvk130 <vanessa.kasula@outlook.com>
2024-05-23 22:41:54 +0300

    Update

---
+++ ServerSentEvents.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from "svelte";
+
+  let messages = [];
+  let eventSource;
+
+  onMount(() => {
+    eventSource = new EventSource("/api/json-sse");
+    // Answer ongoing course hidden
+    };
+  });
+</script>
+
+<main>
+  <h1>Messages</h1>
+  <ul>
+    <!-- Answer ongoing course hidden -->
+  </ul>
+</main>
